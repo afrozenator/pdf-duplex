238aa7e03afd10198f520975282f1608215d8fe9
Afroz Mohiuddin <afrozenator@gmail.com>
2024-06-18 09:07:30 -0700

    fix maybe missing flags, add README

---
+++ README.md
@@ -0,0 +1,13 @@
+# pdf_duplex
+
+I accidentally bought a printer that prints only single sided and not duplex, so
+I wrote a program that will split a pdf into two files, one with odd pages and
+another with even.
+
+Then I print only the odd pages first, and re-insert the pages to print the even
+sides on the same sheets of paper.
+
+The code uses (pypdf)[https://github.com/py-pdf/pypdf] in the simplest possible
+way.
+
+Enjoy!
--- pdf_duplex.py
+++ pdf_duplex.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 import pypdf
 
@@ -8,7 +9,7 @@ Splits a pdf into two, one with odd and the other with even pages.
 This is to print duplex on non duplex printers.
 """
 
-def split_into_two(original_file,
+def split_into_even_odd(original_file,
                    odd_file,
                    even_file):
     orig_pdf = pypdf.PdfReader(original_file)
@@ -21,6 +22,25 @@ def split_into_two(original_file,
     even_writer.write(even_file)
 
 
+def fix_filenames(filename, filename_odd, filename_even):
+    if not filename:
+        raise ValueError("Base filename needs to be given with -f.")
+
+    # Both are present, return early.
+    if filename_odd and filename_even:
+        return filename, filename_odd, filename_even
+
+    base_dir = os.path.dirname(filename)
+
+    # Either or both are absent.
+    if not filename_odd:
+        filename_odd = f'{base_dir}/odd.pdf'
+    if not filename_even:
+        filename_even = f'{base_dir}/even.pdf'
+
+    return filename, filename_odd, filename_even
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
             prog='pdf_duplex',
@@ -30,7 +50,6 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--filename_odd')
     parser.add_argument('-e', '--filename_even')
     args = parser.parse_args()
-    print(args.filename)
-    print(args.filename_odd)
-    print(args.filename_even)
-    split_into_two(args.filename, args.filename_odd, args.filename_even)
+    filename, filename_odd, filename_even = fix_filenames(
+            args.filename, args.filename_odd, args.filename_even)
+    split_into_even_odd(filename, filename_odd, filename_even)
